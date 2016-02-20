beed156ff1f204ab1aadcf0245632b51a9467d88
fproch <test@test.cz>
2016-02-20 20:39:58 +0100

    started work on configuration

--- meetup_sync.js
+++ meetup_sync.js
@@ -1,11 +1,12 @@
 "use strict";
 
+const fs = require('fs');
 const EventEmitter = require('events');
 const request = require('request');
 
 class MeetupSync extends EventEmitter {
 
-    connectMeetupStreams() {
+    connectMeetupStreams(last_mtime) {
         //meetup event stream
         var self = this;
         request('http://stream.meetup.com/2/open_events').on('data', function (e) {
--- sync_firebase.js
+++ sync_firebase.js
@@ -4,18 +4,33 @@
 
 "use strict";
 
+const fs = require('fs');
 const CommunityFirebaseManager = require('./community_fb_manager');
 const MeetupSync = require('./meetup_sync');
 const customFirebaseDefinition = require('./gug_cz_firebase_definition');
 
 var params;
 
-if(process.argv.length < 4) {
+if (process.argv.length < 4) {
     console.log('Missing params, usage:');
     console.log('node sync_firebase.js "<firebaseAppId> <firebaseAppSecret>"');
     return;
 } else {
     params = process.argv.slice(2);
+}
+
+var configFile = '.meetup_firebase_sync_config';
+
+var config;
+try {
+    fs.accessSync(configFile, fs.R_OK | fs.W_OK);
+} catch (e) {
+
+}
+if (fs.exists(configFile)) {
+    config = fs.readFileSync(configFile, 'utf8');
+} else {
+    config = {};
 }
 
 var fbManager = new CommunityFirebaseManager(params[0], params[1], customFirebaseDefinition.dataModel);
